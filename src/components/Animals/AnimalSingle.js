35e4cd53a6e55d73b5de03e8958a4a3894f5e125
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-01 14:36:18 +0200

    added button

--- src/components/Animals/AnimalSingle.js
+++ src/components/Animals/AnimalSingle.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const AnimalSingle = () => {
     let { animal } = useParams();
-    return <div>Meet the {animal}</div>;
+    let history = useHistory();
+    return <div>Meet the {animal}
+    <div>
+        <button onClick={() => history.goBack()}>Back to Animals</button>
+    </div>
+    </div>;
 };
 
 export default AnimalSingle;
